ffe27664a24695574165c2b155ae9bea91db26bd
Gabriel Ortiz <gabriel.ortiz@kanlli.com>
2022-11-04 21:55:27 +0100

    Fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://gabriel-ortiz-99.github.io/',
+  base: 'https://gabriel-ortiz-99.github.io/curso-github-actions/',
   plugins: [react()]
 })
